ddaa4c038c74617cc76fd1fd89594c653db15875
Konstantin <kotplansh@gmail.com>
2023-02-12 20:40:27 +0200

    add message bubbles

---
+++ lib/widgets/chat/message_bubble.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String message;
+  final bool isMe;
+  final Key keys;
+
+  MessageBubble(this.message, this.isMe, {required this.keys});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: isMe
+                ? Colors.grey[300]
+                : Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: isMe ? Radius.circular(12) : Radius.circular(0),
+              bottomRight: !isMe ? Radius.circular(12) : Radius.circular(0),
+            ),
+          ),
+          width: 140,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          child: Text(
+            message,
+            style: TextStyle(color: isMe ? Colors.black : Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,4 +1,6 @@
+import 'package:chat_with_notifications/widgets/chat/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -20,8 +22,10 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocs.length,
-          itemBuilder: (ctx, index) => Text(
+          itemBuilder: (ctx, index) => MessageBubble(
             chatDocs[index]['text'],
+            chatDocs[index]['userId'] == FirebaseAuth.instance.currentUser!.uid,
+            keys: ValueKey(chatDocs[index].id),
           ),
         );
       },
